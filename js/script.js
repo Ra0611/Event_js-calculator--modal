4286d30d7beb8d9ea7cf66d21097dd01ea0a18ae
Ra0611 <an3611nt@gmail.com>
2021-05-23 22:04:25 +0300

    misunderstandings

--- js/script.js
+++ js/script.js
@@ -1,37 +1,55 @@
 
-var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
-var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
-	return new bootstrap.Popover(popoverTriggerEl)
-})
+// var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+// var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+// 	return new bootstrap.Popover(popoverTriggerEl)
+// })
 
 const login = document.getElementById('login')
 const pass = document.getElementById('psw')
 const sub = document.getElementById('sub')
 const mod = document.getElementById('modal')
+const screen = document.getElementById('screen')
 
-login.onchange = function (e) {
-	const test = e.target.value.toString()
-	login.innerHTML = test
-	if (test.length > 3) {
-		login.style.borderColor = 'red'
-	}
-	console.log(test.length)
+// login.onchange = function (e) {
+// 	const test = e.target.value.toString()
+// 	login.innerHTML = test
+// 	if (test.length > 3) {
+// 		login.style.borderColor = 'red'
+// 	}
+// }
+
+// login.onchange = function (e) {
+// 	if (e.target.value == 'ШАГ')
+// 		pass.onchange = function (e) {
+// 			if (e.target.value == 'F03') {
+// 				mod.style.display = 'none'
+// 			} else {
+// 				alert('Неверный пароль!')
+// 			}
+// 		}
+// 	else {
+// 		alert('Неверный логин!')
+// 	}
+// }
+
+function grup(elem) {
+	const number = elem.getAttribute('data-value')
+	const operator = elem.getAttribute('data-operator')
+	screen.innerHTML = number
+
+
+}
+console.log(screen)
+
+
+
+function grup2(elem) {
+	const operator = elem.getAttribute('data-operator')
+	screen.innerHTML = operator
+	console.log(operator)
 }
 
-login.onchange = function (e) {
-	if (e.target.value == 'ШАГ')
-		pass.onchange = function (e) {
-			if (e.target.value == 'F03') {
-				mod.style.display = 'none'
-			} else {
-				alert('Неверный пароль!')
-			}
-		}
-	else {
-		alert('Неверный логин!')
-	}
-}
-console.log(login.value)
+
 
 
 //---------------------------------------------------------------------------------------------------------------------//
